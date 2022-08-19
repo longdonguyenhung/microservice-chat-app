de8e2826cb22ba363a8813e3da5873dad04ab7b3
DickyDicky7 <DickyDicky7@users.noreply.github.com>
2022-08-19 23:35:57 +0700

    😭😭😭

--- gateway_microservice/create_gateway.js
+++ gateway_microservice/create_gateway.js
@@ -1,88 +1,97 @@
 import http from "http";
-import WebSocket, { WebSocketServer } from 'ws';
-import { uuid } from 'uuidv4';
-import pkg from 'query-string';
+import WebSocket, { WebSocketServer } from "ws";
+import { v4 } from "uuid";
+import pkg from "query-string";
 import { query } from "express";
 import fetch from "node-fetch";
-const  querystring  = pkg;
+const querystring = pkg;
 
 var clientMetadata = new Map();
 var userData = new Map();
-const userMappingServiceURL = process.env.SESSION_URL ;
+const userMappingServiceURL =
+  process.env.SESSION_URL || "http://localhost:8083/";
 const serviceAddress = process.env.URL;
-
-/** 
- * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
- * @function
- * @description This function is not really a function it is an endpoint for the connection, it will be tested in integration testing
-*/
-export const createConnection = () => {
-    try{
-        var webserverConnection = new WebSocketServer({port: 8082});
-        acceptWebsocketConnection(webserverConnection);
-        pongMessengerConnection(webserverConnection);
-        closeConnection(webserverConnection);
-    } catch(error) {
-        console.log(error);
-    }
-}
 
 /**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
- * @description establish a connection when receive a request from client. Send a request to update the connection in session service. This should be test on integration testing 
+ * @description This function is not really a function it is an endpoint for the connection, it will be tested in integration testing
+ */
+export const createConnection = () => {
+  try {
+    var webserverConnection = new WebSocketServer({ port: 8082 });
+    acceptWebsocketConnection(webserverConnection);
+    pongMessengerConnection(webserverConnection);
+    closeConnection(webserverConnection);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/**
+ * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
+ * @function
+ * @description establish a connection when receive a request from client. Send a request to update the connection in session service. This should be test on integration testing
  * @param {WebSocket.Server<WebSocket.WebSocket>} webserverConnection
-*/ 
-const acceptWebsocketConnection = (webServerConnection) => {
-    webserverConnection.on('connection', (request, incomingRequest) => {
-        const id = uuid();
-        
-        addClientMetadata(userId, id, request);
-        addUserData(userId, id);
+ */
+const acceptWebsocketConnection = (webserverConnection) => {
+  webserverConnection.on("connection", (request, incomingRequest) => {
+    console.log(incomingRequest.url);
+    const id = v4();
 
-        const updateConnection = createUpdateSessionService(incomingRequest);
-        fetch(userMappingServiceURL + "/session",updateConnection);
+    const userId = querystring.parse(incomingRequest.url.slice(1)).user;
+    addClientMetadata(userId, id, request);
+    addUserData(userId, id);
 
-        request.on('message', (data) => {
-            const messageUpload = sendMessageSessionService(data);
-            fetch(userMappingServiceURL+ "/message" + "?" + new URLSearchParams({topicId: message.topicId}), messageUpload);
-        });
+    const updateConnection = createUpdateSessionService(incomingRequest);
+    fetch(userMappingServiceURL + "/session", updateConnection);
+
+    request.on("message", (data) => {
+      const messageUpload = sendMessageSessionService(data);
+      fetch(
+        userMappingServiceURL +
+          "/message" +
+          "?" +
+          new URLSearchParams({ topicId: message.topicId }),
+        messageUpload
+      );
     });
+  });
 };
 
 /**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @description create a request to session service
  * @param {http.IncomingMessage} webserverConnection
- * @returns {<CustomType>} 
-*/ 
+ * @returns {<CustomType>}
+ */
 const createUpdateSessionService = (incomingRequest) => {
-    const userId = querystring.parse(incomingRequest.url.slice(1)).user;
-    const update = {
-        user: userId,
-        address: serviceAddress
-    }
+  const userId = querystring.parse(incomingRequest.url.slice(1)).user;
+  const update = {
+    user: userId,
+    address: serviceAddress,
+  };
 
-    const updateConnection = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(update)
-    }
-    return updateConnection;
+  const updateConnection = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(update),
+  };
+  return updateConnection;
 };
 
 /**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @description add an key value pair of id to request and userId
- * @function 
+ * @function
  * @param {String} userId
  * @param {String} id
  * @param {WebSocket.WebSocket} request
-*/ 
-const addClientMetadata = (userId, id, request ) => {
-    clientMetadata.set(id, {_request: request, _user: userId});
+ */
+const addClientMetadata = (userId, id, request) => {
+  clientMetadata.set(id, { _request: request, _user: userId });
 };
 
 /**
@@ -91,24 +100,24 @@ const addClientMetadata = (userId, id, request ) => {
  * @function
  * @param {WebSocket.RawData} data
  * @returns {<CustomType>}
-*/ 
+ */
 const sendMessageSessionService = (data) => {
-    const message = JSON.parse(data);
-                
-    const postData = { 
-        type: "data",
-        content: message.content
-    }
+  const message = JSON.parse(data);
 
-    const messageUpload = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postData)
-    }
+  const postData = {
+    type: "data",
+    content: message.content,
+  };
 
-    return messageUpload;
+  const messageUpload = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postData),
+  };
+
+  return messageUpload;
 };
 
 /**
@@ -116,94 +125,90 @@ const sendMessageSessionService = (data) => {
  * @description pong a answer a ping, to keep the connection alive
  * @function
  * @param {WebSocket.Server<WebSocket.WebSocket>} webserverConnection
-*/ 
+ */
 const pongMessengerConnection = (webserverConnection) => {
-    webserverConnection.on('connection', function connection(ws) {
-        ws.on('pong', () => ws.isAlive=true);
-    });
+  webserverConnection.on("connection", function connection(ws) {
+    ws.on("pong", () => (ws.isAlive = true));
+  });
 };
 
 /**
- * @author Long Do Nguyen Hung <hunglong6a1@gmail.com> 
+ * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @description ping to a websocket connection, if the connection not answer after a period of time, terminate it
  * @function
  * @param {WebSocket.Server<WebSocket.WebSocket>} webserverConnection
-*/ 
+ */
 const closeConnection = (webserverConnection) => {
-    const interval = setInterval(function ping() {
+  const interval = setInterval(function ping() {
     webserverConnection.clients.forEach(function each(ws) {
-        if (ws.isAlive === false) return ws.terminate();
-    
-        ws.isAlive = false;
-        ws.ping();
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
     });
-    }, 10000);
-        
-    webserverConnection.on('close', function close() {
-        clearInterval(interval);
-    });
+  }, 10000);
+
+  webserverConnection.on("close", function close() {
+    clearInterval(interval);
+  });
 };
 
-
-
 /**
- * @author Long Do Nguyen Hung <hunglong6a1@gmail.com> 
+ * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @description
  * @function
  * @param {WebSocket.Server<WebSocket.WebSocket>} webserverConnection
-*/ 
+ */
 const addUserData = (userId, id) => {
-    if(userData.has(userId)){
-        const connection = userData.get(userId);
-        connection.push(id);
-        userData.set(userId, connection);
-    } else {
-        const newConnection = [id];
-        userData.set(userId, newConnection);
-    }
-}
+  if (userData.has(userId)) {
+    const connection = userData.get(userId);
+    connection.push(id);
+    userData.set(userId, connection);
+  } else {
+    const newConnection = [id];
+    userData.set(userId, newConnection);
+  }
+};
 
 /**
- * @author Long Do Nguyen Hung <hunglong6a1@gmail.com> 
- * @description delete the connection after the user go offline 
+ * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
+ * @description delete the connection after the user go offline
  * @function
  * @param {WebSocket.Server<WebSocket.WebSocket>} webserverConnection
-*/ 
+ */
 const deleteId = (id) => {
-    if(clientMetadata.has(id)){
-        const user = clientMetadata.get(id)._user;
-        clientMetadata.delete(id);
-        var data = userData.get(user)
-        const index = data.indexOf(id);
-        data.splice(index,1);
-        userData.set(userId, data);
-    }
-}
+  if (clientMetadata.has(id)) {
+    const user = clientMetadata.get(id)._user;
+    clientMetadata.delete(id);
+    var data = userData.get(user);
+    const index = data.indexOf(id);
+    data.splice(index, 1);
+    userData.set(userId, data);
+  }
+};
 
 /**
- * @author Long Do Nguyen Hung <hunglong6a1@gmail.com> 
+ * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @description this is and endpoint, it receive the message from session microservice and send to the correspond connection, if a connection go off, it will delete that connection
  * @function
-*/ 
+ */
 export const receiveMessage = (req, res, next) => {
-    for (let userId of req.body.user) {
-        if(userData.has(userId)){
-            for (const connectionId of userData.get(userId)){.
-                const connection = clientMetadata.get(connectionId)._request;
-                if (connection.readyState === WebSocket.OPEN){
-                    connection.send(req.body.content, { binary: true }, (error) => {
-                        console.log(error);
-                    });
-                } else {
-                    deleteId(connectionId);
-                    continue;
-                }
-            }
+  for (let userId of req.body.user) {
+    if (userData.has(userId)) {
+      for (const connectionId of userData.get(userId)) {
+        const connection = clientMetadata.get(connectionId)._request;
+        if (connection.readyState === WebSocket.OPEN) {
+          connection.send(req.body.content, { binary: true }, (error) => {
+            console.log(error);
+          });
         } else {
-            console.log(userId); //so the connection must be sustain in another service
+          deleteId(connectionId);
+          continue;
         }
+      }
+    } else {
+      console.log(userId); //so the connection must be sustain in another service
     }
-    return res.status(200).send("message success");
-}
-
-
+  }
+  return res.status(200).send("message success");
+};
--- session_microservice/controller/user_mapping.js
+++ session_microservice/controller/user_mapping.js
@@ -2,185 +2,204 @@ import Users from "../model/user.js";
 import Session from "../model/session.js";
 import fetch from "node-fetch";
 import Connection from "../model/connection.js";
-import { Session } from "express-session";
-import { connection } from "mongoose";
+// import { Session } from "express-session";
+// import { connection } from "mongoose";
 
 const userMappingServiceURL = process.env.GROUP_URL;
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description This is an endpoint, when it receive the userId from group_service it send the message to correspond websocket gateway
-*/
+ */
 export const userMapping = async (req, res, next) => {
-    //request the data from group service
-    fetch(userMappingServiceURL + "/group" + "?" + new URLSearchParams({topicId: req.query.topicId})).then((response) => {
-        response.json().then((data) => {
-            data.member.forEach(async (user) => {
+  //request the data from group service
+  fetch(
+    userMappingServiceURL +
+      "/group" +
+      "?" +
+      new URLSearchParams({ topicId: req.query.topicId })
+  ).then((response) => {
+    response.json().then((data) => {
+      data.member.forEach(async (user) => {
+        const userId = await Users.find({ username: user._id }).exec(); //in UserSession model, the username is the userId in User model
 
-                const userId = await Users.find({username: user._id}).exec(); //in UserSession model, the username is the userId in User model
+        //Find the member who own the connections
+        Session.findOne({ member: userId })
+          .populate("member connection")
+          .exec((err, document) => {
+            if (err) {
+              console.log(error);
+              return res.status(400).send(error);
+            }
 
-                //Find the member who own the connections
-                Session.findOne({member: userId}).populate('member connection').exec((err, document) => {
-                    if(err){
-                        console.log(error)
-                        return res.status(400).send(error);
-                    } 
+            if (document && document.member) {
+              sendingDataWebsocket(document, req);
+            }
+          });
+      });
+      return res.status(200).send("success");
+    });
+  });
+};
 
-                    if(document && document.member){
-                        sendingDataWebsocket(document,req);
-                    }
-            });
-        })
-        return res.status(200).send("success");
-    })
-})
-}
-
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description create a body of request to send to the gateway_microservice
  * @param {Session} document Session Mongoose model
  * @param {CustomType} request incoming request
-*/
-const sendingDataWebsocket = (document,request) => {
-    document.connection.forEach((connection) => {
-        const sendingMessage = messageToWebsocket(req.query.topic, req.body.content, document.member.username);
-        fetch("http://" + "websocket-gateway:8081" + "/get_user", sendingMessage).then((respond) => {}, (err) => {});
-    })
-}
+ */
+const sendingDataWebsocket = (document, request) => {
+  document.connection.forEach((connection) => {
+    const sendingMessage = messageToWebsocket(
+      req.query.topic,
+      req.body.content,
+      document.member.username
+    );
+    fetch(
+      "http://" + "websocket-gateway:8081" + "/get_user",
+      sendingMessage
+    ).then(
+      (respond) => {},
+      (err) => {}
+    );
+  });
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description create a body of request to send to the gateway_microservice
  * @param {String} topicId id of the topic
  * @param {String} content message send to the gateway_microservice
  * @param {String} userId id of user who receive message
-*/
+ */
 const messageToWebsocket = (topicId, content, userId) => {
-    const sendingData = {
-        topic: topicId,
-        content: content,
-        user: [userId]
-    }
-    
-    const sendingMessage = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(sendingData)
-    }
-    return sendingMessage;
+  const sendingData = {
+    topic: topicId,
+    content: content,
+    user: [userId],
+  };
+
+  const sendingMessage = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sendingData),
+  };
+  return sendingMessage;
 };
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @async
- * @description This function update user connection. 
+ * @description This function update user connection.
  * @param {String} address this is an endpoint, it will be tested in integration testing
  * @return {Promise} this will return a Promise which resolve to a user or null
-*/
-export const updateConnection = async (req,res, next) => {
-    const username = req.body.user;
-    const address = req.body.address;
+ */
+export const updateConnection = async (req, res, next) => {
+  const username = req.body.user;
+  const address = req.body.address;
 
-    const connection = await createNewConnection(address);
-    const user = await findUser(username);
+  const connection = await createNewConnection(address);
+  const user = await findUser(username);
 
-    const connectionId = connection._id;
-    const userId = user._id;
-    if(!userId){
-        return createNewUser(user).then(async (user) => createNewSession(userId, connectionId))
-                .then((session) => {return res.status(200).json(session)});
-    } else {
-        const data = updateSessionConnection(userId, connectionId);
-        return res.status(400).json(data);
-    }
-    
-}
+  const connectionId = connection._id;
+  const userId = user._id;
+  if (!userId) {
+    return createNewUser(user)
+      .then(async (user) => createNewSession(userId, connectionId))
+      .then((session) => {
+        return res.status(200).json(session);
+      });
+  } else {
+    const data = updateSessionConnection(userId, connectionId);
+    return res.status(400).json(data);
+  }
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description This function find user
  * @param {String} address this is the address of new websocket connection
  * @return {Promise} this will return a Promise which resolve to a user or null
-*/
+ */
 const findUser = (user) => {
-    return Users.findOne({username: user}).exec()
-}
+  return Users.findOne({ username: user }).exec();
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description This function create a new Connection instance
  * @param {String} address this is the address of new websocket connection
  * @return {Promise} this will return a Promise which resolve to new Connection
-*/
+ */
 const createNewConnection = (address) => {
-    return Connection.create({address: address});
-}
+  return Connection.create({ address: address });
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description This function create a new User instance
  * @param {String} user unique userId use as a username in UserSession instance
  * @return {Promise} this will return a Promise which resolve to new UserSession
-*/
+ */
 const createNewUser = (user) => {
-    return Users.create({username: user})
-}
+  return Users.create({ username: user });
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @description This function create a new Session instance
  * @param {String} user unique userId use as a username in UserSession instance
  * @param {String} connectionId the id of the new created connection
  * @return {Promise} this will return a Promise which resolve to new Session
-*/
+ */
 const createNewSession = (userId, connectionId) => {
-    return Session.create({connection: [connectionId], member: user});
-}
+  return Session.create({ connection: [connectionId], member: user });
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @async
  * @description This function create a new User instance and new Session instance
  * @param {String} id id of the Session instance
  * @param {String} connectionId the id of the new created connection
-*/
+ */
 const updateConnectionWhenSessionNotExist = (id, connectionId) => {
-    Session.updateOne({_id:id}, { $push: {connection: connectionId}});
-}
+  Session.updateOne({ _id: id }, { $push: { connection: connectionId } });
+};
 
-/** 
+/**
  * @author Long Do Nguyen Hung <hunglong6a1@gmail.com>
  * @function
  * @async
  * @description This function update a new connection
  * @param {String} userId id of the SessionUser
- * @param {String} connectionId id 
- * @return {Session} it will return a Session instance 
-*/
+ * @param {String} connectionId id
+ * @return {Session} it will return a Session instance
+ */
 const updateSessionConnection = (userId, connectionId) => {
-    Session.findOne({member:userId} ).populate('member').exec((err, data) => {
-        if(err)
-            console.log(err);
-        else {
-            if(data !== null){
-                updateConnectionWhenSessionNotExist(data._id, connectionId);
-                return data;
-            } else {
-                const newSession = await createNewSession(userId, connectionId);
-                return newSession;
-            }
+  Session.findOne({ member: userId })
+    .populate("member")
+    .exec(async (err, data) => {
+      if (err) console.log(err);
+      else {
+        if (data !== null) {
+          updateConnectionWhenSessionNotExist(data._id, connectionId);
+          return data;
+        } else {
+          const newSession = await createNewSession(userId, connectionId);
+          return newSession;
         }
-    }); 
-}
+      }
+    });
+};
