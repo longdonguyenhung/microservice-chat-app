f9ccfdebe7723998feb6acc467e338f2ba620117
Nhat Hoang <nhathv@beetsoft.com.vn>
2022-08-20 09:56:34 +0700

    Add Dockerfile

---
+++ notification_service/src/test/java/notificationservice/chatproject/ChatprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package notificationservice.chatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
